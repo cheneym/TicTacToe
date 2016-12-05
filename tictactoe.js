8708809d70747cb43ad1c866af1f4e4961e5bbe1
Cheney Mao <cheneymao94@gmail.com>
2016-12-05 15:30:09 -0800

    Add coordinate system

--- tictactoe.js
+++ tictactoe.js
@@ -104,13 +104,14 @@ const updateBoard = (x, y) => {
 };
 
 const displayBoard = () => {
-  const boardDisplay = `-------
-|${board[0][0]}|${board[0][1]}|${board[0][2]}|
--------
-|${board[1][0]}|${board[1][1]}|${board[1][2]}|
--------
-|${board[2][0]}|${board[2][1]}|${board[2][2]}|
--------`;
+  const boardDisplay = ` -------
+2|${board[0][0]}|${board[0][1]}|${board[0][2]}|
+ -------
+1|${board[1][0]}|${board[1][1]}|${board[1][2]}|
+ -------
+0|${board[2][0]}|${board[2][1]}|${board[2][2]}|
+y-------
+ x0 1 2`;
   console.log(boardDisplay);
 
 };
