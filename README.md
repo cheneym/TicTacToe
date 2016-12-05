07b02c6962cc3934a7bf1bb4753a1876a13b6228
Cheney Mao <cheneymao94@gmail.com>
2016-12-05 15:08:19 -0800

    Implement ability to add X and O to board

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TicTacToe
+How to start game:
+1. clone repository
+2. run: npm install
+3. run: node tictactoe.js
---
+++ tictactoe.js
@@ -0,0 +1,74 @@
+const prompt = require('prompt');
+
+const board = [];
+for (let i = 0; i < 3; i++) {
+  board[i] = [];
+  for (let j = 0; j < 3; j++) {
+    board[i][j] = ' ';
+  }
+}
+
+let p1Turn = true;
+
+const promptUser = () => {
+  if (p1Turn) {
+    console.log('It is player 1\'s turn');
+  } else {
+    console.log('It is player 2\'s turn');
+  }
+  prompt.start();
+  prompt.get(['xPosition', 'yPosition'], (err, result) => {
+    if (err) { return err; }
+    const x = +result.xPosition;
+    const y = +result.yPosition;
+    if (x > 2 || x < 0 || y > 2 || y < 0) {
+      console.log('Invalid coordinates');
+      promptUser();
+    } else if (!positionChecker(x, y)) {
+      console.log('Coordinates are taken');
+      promptUser();
+    } else {
+      updateBoard(x, y);
+    }
+  });
+}
+
+const startGame = () => {
+  displayBoard();
+  promptUser();
+};
+
+const positionChecker = (x, y) => {
+  if (board[2-y][x] === ' ') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+const updateBoard = (x, y) => {
+  if (p1Turn) {
+    board[2-y][x] = 'X';
+    p1Turn = false;
+  } else {
+    board[2-y][x] = 'O';
+    p1Turn = true;
+  }
+  startGame();
+};
+
+const displayBoard = () => {
+  const boardDisplay = `-------
+|${board[0][0]}|${board[0][1]}|${board[0][2]}|
+-------
+|${board[1][0]}|${board[1][1]}|${board[1][2]}|
+-------
+|${board[2][0]}|${board[2][1]}|${board[2][2]}|
+-------`;
+  console.log(boardDisplay);
+
+};
+
+startGame();
